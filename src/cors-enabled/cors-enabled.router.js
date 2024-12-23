17626efa76fc7a7a6a321cb989ef8e1ab7729c37
kernel528 <kernel528@gmail.com>
2024-12-22 21:13:47 -0600

    Updated cors-enabled.router.js to support CORS on GET and DELETE methods examples.

--- src/cors-enabled/cors-enabled.router.js
+++ src/cors-enabled/cors-enabled.router.js
@@ -1,18 +1,24 @@
 const router = require("express").Router();
 const controller = require("./cors-enabled.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
+const corsDelete = cors({methods: "DELETE"});
 
 router
   .route("/:corsId")
-  .get(controller.read)
-  .put(controller.update)
-  .delete(controller.delete)
-  .all(methodNotAllowed);
+    .get(controller.read)
+    .put(controller.update)
+    // .delete(controller.delete) // This was the initial configuration.
+    // .delete(cors(), controller.delete) // non-options enabled in module.
+    .delete(corsDelete, controller.delete)
+    .options(corsDelete)
+    .all(methodNotAllowed);
 
 router
   .route("/")
-  .get(controller.list)
-  .post(controller.create)
-  .all(methodNotAllowed);
+    // .get(controller.list)
+    .get(cors(), controller.list)
+    .post(controller.create)
+    .all(methodNotAllowed);
 
 module.exports = router;
