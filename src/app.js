bfd21b7af1cf249c2f3267c784ed7e8143c6375b
kernel528 <kernel528@gmail.com>
2024-12-22 21:34:13 -0600

    Updated to enable CORS on all of api as part of module tasks.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors"); // module 3.12.3, enabling for entire api
 const app = express();
 
 const corsEnabledRouter = require("./cors-enabled/cors-enabled.router");
@@ -7,6 +8,7 @@ const corsNotEnabledRouter = require("./cors-not-enabled/cors-not-enabled.router
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
 
+app.use(cors()); // module 3.12.3, enabling for api.
 app.use(express.json());
 
 app.use("/cors-enabled", corsEnabledRouter);
